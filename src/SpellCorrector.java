d0aaa6ab7b7fcaa4c02f6d87088e81c4603e5119
Cryptorr <denhartog.sven@gmail.com>
2015-03-24 10:57:16 +0100

    getSmootherCount update

--- src/SpellCorrector.java
+++ src/SpellCorrector.java
@@ -29,16 +29,16 @@ public class SpellCorrector {
         String finalSuggestion = "";
         
         /** CODE TO BE ADDED **/
+        double perplexity;
+        
         for (int i = 0; i < words.length; i++){
             double bestvalue = 0;
             String bestword = words[i];
             
             for (String word : getCandidateWords(words[i])){
                 double likelihood = calculateChannelModelProbability(word, words[i]);
-                double prior = cr.getSmoothedCount(word)/cr.getVocabularySize();
-                double preNGramCount = i==(words.length-1)  ? 1 : cr.getSmoothedCount(words[i-1] + " " + word);
-                double postNGramCount = i==0                ? 1 : cr.getSmoothedCount(word + " " + words[i+1]);
-                double twoGramCount = preNGramCount*postNGramCount;
+                double prior = cr.getSmoothedCount(word);
+                double NGramCount = i==(words.length-1)  ? 1 : cr.getSmoothedCount(words[i-1] + " " + word);
                 double wcorrect = SCALE_FACTOR * likelihood * Math.pow(prior, LAMBDA);
 //                if ( > bestvalue){
 //                    bestword = word;
